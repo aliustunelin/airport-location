88829c328860a78b528d774413e7e90cb3b52c60
aliustunelin <aliustunelin@gmail.com>
2024-02-20 09:26:28 +0300

    sql not test

--- django_main/location/management/commands/load_custom_data.py
+++ django_main/location/management/commands/load_custom_data.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from ...models import Airport, City, Country
 import json
 import os
+import sqlite3
 
 
 class Command(BaseCommand):
@@ -15,17 +16,31 @@ class Command(BaseCommand):
                 self.stdout.write(self.style.HTTP_INFO('This is true state'))
             else:
                 self.stdout.write(self.style.HTTP_INFO(f'Error: File not found at path {json_cities_file_path}'))
+
+
+            conn = sqlite3.connect('project.db')
+            cursor = conn.cursor()
+
+
             with open(json_cities_file_path, 'r') as json_file:
                 self.stdout.write(self.style.HTTP_INFO('now here'))
                 data = json.load(json_file)
                 
                 
                 for item in data:
+                    name=item['name']
+
                     Country.objects.create(
-                        name=item['name'],
-                        
+                        name=name,
                     )
+                    cursor.execute(f"INSERT INTO Products (name) VALUES (?)",
+                     (Country.name))
+                    
+                    conn.commit()
 
             self.stdout.write(self.style.SUCCESS('Successfully  cities loaded custom data.'))
         except Exception as e:
+            conn.rollback()
             self.stdout.write(self.style.ERROR(f'Error loading custom data: {str(e)}'))
+        finally:
+            conn.close()
